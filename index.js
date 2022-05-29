adbd04177f0dce3c81376f624050ca550d1ab7e3
hexaalif <hexaalif2020@gmail.com>
2022-05-29 13:21:24 +0600

    products api called

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        await client.connect()
+        const productsCollection = client.db('homies').collection('products');
+
+        app.get('/products', async(req, res) =>{
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally{
+        // ok
+    }
+}
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
   res.send('Hello From Homies!')
